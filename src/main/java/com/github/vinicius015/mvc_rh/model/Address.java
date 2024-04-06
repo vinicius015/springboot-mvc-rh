dde8ae960a8b176eb8022f04dcb6a57fd3eb9421
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-05 21:43:44 -0300

    created address model

---
+++ src/main/java/com/github/vinicius015/mvc_rh/model/Address.java
@@ -0,0 +1,99 @@
+package com.github.vinicius015.mvc_rh.model;
+
+import jakarta.persistence.*;
+
+import static jakarta.persistence.GenerationType.AUTO;
+
+@Entity
+@Table(name = "addresses")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private Long id;
+
+    @Column(nullable = false, length = 60)
+    private String neighborhood;
+
+    @Column(nullable = false, length = 9)
+    private String postalCode;
+
+    @Column(nullable = false, length = 60)
+    private String city;
+
+    @Column(length = 60)
+    private String addressLineTwo;
+
+    @Column(nullable = false, length = 150)
+    private String address;
+
+    @Column(nullable = false, length = 60)
+    private String state;
+
+    @Column(length = 5)
+    private Long number;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNeighborhood() {
+        return neighborhood;
+    }
+
+    public void setNeighborhood(String neighborhood) {
+        this.neighborhood = neighborhood;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddressLineTwo() {
+        return addressLineTwo;
+    }
+
+    public void setAddressLineTwo(String addressLineTwo) {
+        this.addressLineTwo = addressLineTwo;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Long getNumber() {
+        return number;
+    }
+
+    public void setNumber(Long number) {
+        this.number = number;
+    }
+}
